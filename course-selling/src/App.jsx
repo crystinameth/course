c8f98d71bedabed7c60f981fd1461b406a322c40
shivimishra <shivimishra130103@gmail.com>
2023-11-27 21:53:16 +0530

    frontend setup

---
+++ course-selling/src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Landing from "./components/Landing";
+import CreateCourse from './components/CreateCourse';
+import Register from './components/Register';
+import ShowCourses from './components/ShowCourses';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/about" element={<CreateCourse />} />
+                <Route path="/courses" element={<ShowCourses />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
